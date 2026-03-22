3bbd136eafb4d99786f1870ecb8d26be8e6daa25
Yoni <yonatan.alexanderch@gmail.com>
2026-03-22 15:13:06 -0300

    Add comments in check_charger_status script

--- check_charger_status.sh
+++ check_charger_status.sh
@@ -1,12 +1,17 @@
 #! /bin/bash
+
+# Script to notify changes on battery status.
+
 PREVIOUS=""
 while true; do
     STATUS=$(cat /sys/class/power_supply/BAT1/status)
+
+    # If there was a change on battery status
     if [ "$STATUS" != "$PREVIOUS" ]; then
         if [ "$STATUS" = "Not charging" ]; then
-            notify-send "󱟢 Battery" "Full Battery"
+            notify-send "󱟢 Battery" "Full Battery" # Notify when it's fully charged
         else
-            notify-send "󰂄 Battery" "$STATUS Battery"
+            notify-send "󰂄 Battery" "$STATUS Battery" # Notify when the status changed (you connect or disconnect the power source)
         fi
         PREVIOUS="$STATUS"
     fi
